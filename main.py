6af996ff77167c2c725ab6557a400ca447cb9701
altrono <hbotema16@gmail.com>
2023-01-24 15:59:34 +0200

    Boiler Plate Setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+
+screen = Screen()
+screen.bgcolor('black')
+screen.title('Altrono Pong Game')
+screen.setup(width=600, height=600)
+
+
+
+
+
+
+
+screen.exitonclick()
